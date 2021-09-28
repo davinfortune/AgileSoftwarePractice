15ece4ab5ad852ba51c052f1815cf38c09d7113c
Davin Fortune (20085000) <20085000@wit.ie>
2021-09-28 09:40:22 +0100

    Check reorders fully implemented and tested

--- Exercises/mochaLab/test/catalogue-test.js
+++ Exercises/mochaLab/test/catalogue-test.js
@@ -7,6 +7,7 @@ let cat = null;
 let batch = null;
 
 describe("Catalogue", () => {
+
   beforeEach( () => {
     cat = new Catalogue("Test Catalogue");
     cat.addProduct(new Product("A123", "Product 1", 100, 10, 10.0));
@@ -35,5 +36,28 @@ describe("Catalogue", () => {
       const result = cat.removeProductById("A321");
       expect(result).to.be.undefined;
     });
+  });
+
+  describe("checkReorder", () => {
+    it("should return an empty array when no products need reordering", function () {
+      const result = cat.checkReorders();
+      expect(result.productIds).to.be.empty;
+    });
+    it("should report products that satisfy reorder criteria", function () {
+      cat.addProduct(new Product("B123", "Product 4", 10, 20, 10.0));
+      cat.addProduct(new Product("B124", "Product 5", 10, 30, 10.0));
+      const result = cat.checkReorders();
+      expect(result.productIds).to.have.lengthOf(2);
+      expect(result.productIds).to.have.members(["B123", "B124"]);
+    });
+    it("should include products just on their reorder level", function () {
+      cat.addProduct(new Product("B125", "Product 6", 10, 10, 10.0));
+      const result = cat.checkReorders();
+      expect(result.productIds).to.have.members(["B125"]);
+    });
+    it("should handle an empty catalogue", function () {
+        const result = cat.checkReorders();
+       expect(result.productIds).to.have.lengthOf(0);
+     });
   });
 });
