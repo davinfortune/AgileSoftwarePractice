9b83405865527cfcfd2dbbc0b3e417844b1f4030
Davin Fortune (20085000) <20085000@wit.ie>
2021-11-02 10:49:17 +0000

    E2E tests - Movie Details page - poster images.

--- cypress/integration/movieDetails-page-spec.js
+++ cypress/integration/movieDetails-page-spec.js
@@ -19,21 +19,25 @@ describe("Movie Details Page", () => {
     cy.visit(`/movies/${movie.id}`);
   });
  
-  it("should display movie title in the page header", () => {
-    cy.get("h3").contains(movie.title);
-});
-
-it("should display the movie's details", () => {
-  cy.get("h3").contains("Overview");
-  cy.get("h3").next().contains(movie.overview);
-  cy.get("ul")
-    .eq(1)
-    .within(() => {
-      const genreChips = movie.genres.map((g) => g.name);
-      genreChips.unshift("Genres");
-      cy.get("span").each(($card, index) => {
-        cy.wrap($card).contains(genreChips[index]);
-      });
+    it("should display movie title in the page header", () => {
+        cy.get("h3").contains(movie.title);
     });
-});
+
+    it("should display the movie's details", () => {
+        cy.get("h3").contains("Overview");
+        cy.get("h3").next().contains(movie.overview);
+        cy.get("ul")
+            .eq(1)
+            .within(() => {
+            const genreChips = movie.genres.map((g) => g.name);
+            genreChips.unshift("Genres");
+            cy.get("span").each(($card, index) => {
+                cy.wrap($card).contains(genreChips[index]);
+            });
+            });
+        });
+
+    it("should display movie posters on the left side", () => {
+        cy.get('img').should('have.css', 'text-align', 'left');
+    });
 });
