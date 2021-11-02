122c08614bfdcb6b091f4f682785eccefdcd4c95
Davin Fortune (20085000) <20085000@wit.ie>
2021-11-02 09:45:42 +0000

    E2E tests - Home page - Base test.

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# AgileSoftwarePractice
+# Week7
 
-This is the labs for the module *Agile Software Practice* Compiled into one repo, each branch represents a Weeks lab!!
-<br/><br/><br/><br/>
-<p align="center">
-  <img width="460" height="300" src="https://media4.giphy.com/media/gw3IWyGkC0rsazTi/giphy.gif?cid=ecf05e471e0ek38bajxhtknp3jehbg7w8t80ejgrkr97su4x&rid=giphy.gif&ct=g">
-</p>
+This lab will use the Cypress testing framework to automate the End-to-End (E2E) testing of the movies fan app from the Web App development module.
---
+++ src/api/tmdb-api.js
@@ -0,0 +1,59 @@
+export const getMovies = () => {
+    var key = "77885d4f621d9af0c6c5c522b1c9df9d";
+    return fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&include_adult=false&page=1`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
+  };
+  
+  export const getMovie = id => {
+    var key = "77885d4f621d9af0c6c5c522b1c9df9d";
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${key}`
+    ).then(res => res.json());
+  };
+  
+  export const getGenres = () => {
+    var key = "77885d4f621d9af0c6c5c522b1c9df9d";
+    return fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+        key +
+        "&language=en-US"
+    )
+      .then(res => res.json())
+      .then(json => json.genres);
+  };
+  
+  export const getMovieImages = (id) => {
+    var key = "77885d4f621d9af0c6c5c522b1c9df9d";
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${key}`
+    )
+      .then((res) => res.json())
+      .then((json) => json.posters);
+  };
+
+  export const getMovieReviews = (id) => {
+    var key = "77885d4f621d9af0c6c5c522b1c9df9d";
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${key}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.results);
+        return json.results;
+      });
+  };
+
+  export const getMovieUpcoming = () => {
+    var key = "77885d4f621d9af0c6c5c522b1c9df9d";
+    return fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json.results);
+        return json.results;
+      });
+  };
